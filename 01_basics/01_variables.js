7af8efa47627cbdacdf9a24176528a7a4831f438
adarshhembram <139600159+adarshhembram@users.noreply.github.com>
2023-11-06 02:54:14 +0000

    done config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 159116;
+let accountEmail = "adarsh@google.com"
+var accountpassword = "11254202"
+acoouuntCity="Dhanbad"
+let accountState;
+
+
+// accountId = 2 // not allowed
+
+accountEmail = "guddu@gmail.com"
+accountpassword= "8764680"
+acoouuntCity = "Ramgarh"
+
+/*
+    prefer not to use var
+    bacause of issue in block scope and functional scope
+*/
+console.log(accountEmail);
+console.table([accountEmail,accountpassword,acoouuntCity,accountState]) 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code in new version
+
+// aleart (3+3)// we are using nolog    
+console.log(3 +
+    3); // code reasobality should be heigh
+
+console.log("adarsh");
+
+let name = "Adarsh"
+let age = 18
+let isloggedIn = false
+
+// number - 2 to power 53
+// bigint
+// string
+// boolean
+// null - standa lone value
+// undefined
+// symbol - unique
+// object
+
+
+console.log(typeof age);
