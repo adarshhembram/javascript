19be37fb5b6402a9ae415859cfde253449c0c9b4
adarshhembram <139600159+adarshhembram@users.noreply.github.com>
2023-11-08 00:28:11 +0000

    data types summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,33 @@
+// PRIMITIVE
+
+//7 Types : string,number,boolean,nill,udefined, symbol, Bigint
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+
+
+// Reference (Non Primitive)
+
+// array, objects, Functions
+
+const heros = [" shaktiman" , "Doremon"]
+
+let myObj {
+    name: "Adarsh",
+    age : 22,
+}
+
+console myFunction = Functions(){
+    console.log("hello world");
+}
