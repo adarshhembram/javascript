868dafc04d51dd23055ac33d90234c4ed6355fbc
adarshhembram <139600159+adarshhembram@users.noreply.github.com>
2023-11-09 00:03:48 +0000

    complete

--- 01_basics/05_String.js
+++ 01_basics/05_String.js
@@ -4,3 +4,14 @@ const repoCount = 50
 // console.log(name + repoCount);
 
 console.log('hello my name is $ {name} and my repo count is ${repoCount}');
+
+const gameName = new String('Adarsh')
+
+console.log(gameName[0]);
+
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
