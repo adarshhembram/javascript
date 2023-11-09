32109cc71d41055093609d0c5f24cb0e6237d8eb
adarshhembram <139600159+adarshhembram@users.noreply.github.com>
2023-11-09 00:52:40 +0000

    07complete

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,11 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+let myCreateDate = new Date(2023,100,9)
+console.log(myCreateDate.toLocaleString);
